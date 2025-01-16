3d36edf801ca8258b43cc033413ec75bee62ca13
pranayh24 <pranayheda24@gmail.com>
2025-01-16 23:50:59 +0530

    Add JPA dependencies and entities

--- src/main/java/org/url/shortener/UrlShortenerApplication.java
+++ src/main/java/org/url/shortener/UrlShortenerApplication.java
@@ -1,13 +1,13 @@
-package org.url.shortner;
+package org.url.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlShortnerApplication {
+public class UrlShortenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UrlShortnerApplication.class, args);
+        SpringApplication.run(UrlShortenerApplication.class, args);
     }
 
 }
