89392ae3d8394a53d61d733b4e950fb9b27eb0bf
pranayh24 <pranayheda24@gmail.com>
2025-01-26 23:54:01 +0530

    updated the register function to register users via the frontend

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -6,6 +6,8 @@ import AboutPage from "./components/AboutPage.jsx";
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 import RegisterPage from './components/RegisterPage.jsx';
+import { Toaster } from 'react-hot-toast';
+import Login from './components/Login.jsx';
 
 function App() {
 
@@ -13,10 +15,12 @@ function App() {
     <>
       <Router>
       <NavBar />
+      <Toaster position='bottom-center'/>
         <Routes>
           <Route path='/' element={<LandingPage />}/>
           <Route path='/about' element={<AboutPage />}/>
           <Route path='/register' element={<RegisterPage />}/>
+          <Route path='/login' element={<Login />}/>
         </Routes>
         <Footer />
       </Router>
--- url-shortener-frontend/src/components/RegisterPage.jsx
+++ url-shortener-frontend/src/components/RegisterPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import TextField from './TextField'
+import api from '../api/api'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 const RegisterPage = () => {
+
+    const navigate = useNavigate();
+	const [loader,setLoader] = useState(false);
+
     const {
         register,
         handleSubmit,
@@ -17,6 +26,21 @@ const RegisterPage = () => {
     });
 
     const registerHandler = async(data) => {
+		setLoader(true);
+        try {
+            const{ data : response } = await api.post(
+                "api/auth/public/register",
+                data
+            );
+            reset();
+            navigate("/login");
+            toast.success("Registeration Successful!");
+        } catch(error) {
+            console.log(error);
+            toast.error("Registration Failed! PLease try again");
+        } finally {
+            setLoader(false);
+        }
     };
 
   return (
@@ -27,6 +51,61 @@ const RegisterPage = () => {
             <h1 className="text-center font-serif text-btnColor font-bold lg:text-xl text-xl">
                 Register
             </h1>
+
+            <hr className="mt-2 mb-5 text-black"/>
+
+            <div className="flex flex-col gap-3">
+                <TextField
+                label="Username"
+                id="username"
+                type="text"
+                errors={errors}
+                register={register}
+                required={true}
+                message="Username is required"
+                placeholder="Enter your username"
+                />
+
+                <TextField
+                label="Email"
+                id="email"
+                type="email"
+                errors={errors}
+                register={register}
+                required={true}
+                message="Email is required"
+                placeholder="Enter your email"
+                />
+
+                <TextField
+                label="Password"
+                id="password"
+                type="password"
+                errors={errors}
+                register={register}
+                min = {6}
+                required={true}
+                message="Password is required"
+                placeholder="Enter your password"
+                />
+
+            </div>
+
+            <button
+				disabled={loader}
+                type="submit"
+				className='bg-customRed font-semibold text-white  bg-custom-gradient w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3'>
+					{loader ? "Loading..." : "Register"}
+
+            </button>
+
+			<p className='text-center text-sm text-slate-600 mt-6'>
+				Already have an account? 
+				<Link className='font-semibold underline hover:text-black'
+				to="/login">
+					<span className="text-btnColor"> Login</span>
+				</Link>
+			</p>
         </form>
     </div>
   )
